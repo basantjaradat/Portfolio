079fda4fc22f8c0568d4a05c54f763bd4dea9dba
Basant Taher Jaradat <132229745+basantjaradat@users.noreply.github.com>
2025-07-23 13:54:07 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Home() {
 
   return (
-    <div>
+    <div className="responsive-container">
       <div className="navbar-box">
         <span>BASANT JARADAT PORTFOLIO</span>
         <div className="menu">
