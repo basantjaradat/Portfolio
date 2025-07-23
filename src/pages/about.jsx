d1502fbc5edc695ce03e817625d94a188ace7120
Basant Taher Jaradat <132229745+basantjaradat@users.noreply.github.com>
2025-07-23 14:05:47 +0300

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,7 @@ const About = () => {
   const { about, education, experience, skills, languages } = profileData;
 
   return (
-    <div className="responsive-container">
+    <div >
       <div className="navbar-box">
         <span>BASANT JARADAT PORTFOLIO</span>
         <div className="menu">
